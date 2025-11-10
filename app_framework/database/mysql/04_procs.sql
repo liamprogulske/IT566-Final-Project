f7ae7f4b09f1b6c9e903633ace63c65f86dc1ca9
liamprogulske <97378485+liamprogulske@users.noreply.github.com>
2025-11-10 12:23:21 -0500

    Create stored procedures for campaign and channel retrieval
    Added stored procedures to retrieve campaigns for a channel and channels for a campaign.

---
+++ app_framework/database/mysql/04_procs.sql
@@ -0,0 +1,25 @@
+
+-- 04_procs.sql (MySQL)
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS sp_get_campaigns_for_channel $$
+CREATE PROCEDURE sp_get_campaigns_for_channel(IN p_channel_id INT)
+BEGIN
+  SELECT c.*
+  FROM campaign c
+  JOIN campaign_channel_xref x ON x.campaign_id = c.campaign_id
+  WHERE x.channel_id = p_channel_id
+  ORDER BY c.name;
+END $$
+
+DROP PROCEDURE IF EXISTS sp_get_channels_for_campaign $$
+CREATE PROCEDURE sp_get_channels_for_campaign(IN p_campaign_id INT)
+BEGIN
+  SELECT ch.*
+  FROM channel ch
+  JOIN campaign_channel_xref x ON x.channel_id = ch.channel_id
+  WHERE x.campaign_id = p_campaign_id
+  ORDER BY ch.name;
+END $$
+
+DELIMITER ;
