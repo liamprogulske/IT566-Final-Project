6450c1efcf6cf2b759737580712dc6a9091974d3
liamprogulske <97378485+liamprogulske@users.noreply.github.com>
2025-11-10 12:21:10 -0500

    Add teardown script for campaign and channel tables
    Remove views and tables related to campaigns and channels.

---
+++ app_framework/database/sqlite/99_teardown.sql
@@ -0,0 +1,7 @@
+
+-- 99_teardown.sql (SQLite)
+DROP VIEW IF EXISTS v_channel_campaign_counts;
+DROP VIEW IF EXISTS v_campaign_channel_counts;
+DROP TABLE IF EXISTS campaign_channel_xref;
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS channel;
