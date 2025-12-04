97ab0435c4262d06a053189f3b2065e0ed777f49
liamprogulske <97378485+liamprogulske@users.noreply.github.com>
2025-12-04 15:47:45 -0500

    Delete app_framework/database/sqlite directory

--- app_framework/database/sqlite/99_teardown.sql
+++
@@ -1,7 +0,0 @@
-
--- 99_teardown.sql (SQLite)
-DROP VIEW IF EXISTS v_channel_campaign_counts;
-DROP VIEW IF EXISTS v_campaign_channel_counts;
-DROP TABLE IF EXISTS campaign_channel_xref;
-DROP TABLE IF EXISTS campaign;
-DROP TABLE IF EXISTS channel;
